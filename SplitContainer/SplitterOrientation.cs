69b01b35624d01077bb8196c67f640808dccdb4f
jsakamoto <junichi.jp@gmail.com>
2024-09-09 21:38:45 +0900

    Marked the old version component as deprecated.

--- SplitContainer/SplitterOrientation.cs
+++ SplitContainer/SplitterOrientation.cs
@@ -1,8 +1,11 @@
-﻿namespace Toolbelt.Blazor.Splitter;
+﻿using System.ComponentModel;
+
+namespace Toolbelt.Blazor.Splitter;
 
 /// <summary>
 /// Specifies the orientation of the spliter.
 /// </summary>
+[Obsolete("Use the one in the Toolbelt.Blazor.Splitter.V2 namespace instead."), EditorBrowsable(EditorBrowsableState.Never)]
 public enum SplitterOrientation
 {
     /// <summary>
