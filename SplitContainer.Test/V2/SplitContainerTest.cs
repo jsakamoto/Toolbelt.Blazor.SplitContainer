32a6603477ddfc7a9de8b8e5dbf59d3bb24291da
Damian <warnaa@gmail.com>
2026-05-25 16:23:35 +0200

    Add tests first

--- SplitContainer.Test/V2/SplitContainerTest.cs
+++ SplitContainer.Test/V2/SplitContainerTest.cs
@@ -1,4 +1,5 @@
-﻿using Toolbelt.Blazor.Splitter.V2;
+﻿using System.Globalization;
+using Toolbelt.Blazor.Splitter.V2;
 
 namespace SplitContainer.Test.V2;
 
@@ -70,5 +71,89 @@ public class SplitContainerTest
 
         // Then
         styleText.Is("min-width:calc(8.5% - calc(var(--splitter-bar-size) / 2));flex:1;");
+    }
+}
+
+public class SplitContainerCultureTest
+{
+    private CultureInfo _OriginalCulture = null!;
+    private CultureInfo _OriginalUICulture = null!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        this._OriginalCulture = CultureInfo.CurrentCulture;
+        this._OriginalUICulture = CultureInfo.CurrentUICulture;
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        CultureInfo.CurrentCulture = this._OriginalCulture;
+        CultureInfo.CurrentUICulture = this._OriginalUICulture;
+    }
+
+    // The browser's CSS calc() only accepts '.' as the decimal separator,
+    // so the style must never contain ',' regardless of the OS / app culture.
+    [TestCase("pl-PL")]
+    [TestCase("de-DE")]
+    [TestCase("fr-FR")]
+    [TestCase("ru-RU")]
+    [TestCase("nl-NL")]
+    public void GetPaneStyle_PercentSize_UsesInvariantDecimalSeparator(string cultureName)
+    {
+        CultureInfo.CurrentCulture = new CultureInfo(cultureName);
+        CultureInfo.CurrentUICulture = CultureInfo.CurrentCulture;
+
+        var splitContainer = new SplitContainer<double>
+        {
+            Orientation = SplitterOrientation.Horizontal,
+            UnitOfPaneSize = UnitOfPaneSize.Percent,
+        };
+
+        var styleText = splitContainer.GetPaneStyle(47.059, null);
+
+        styleText.Is("height:calc(47.059% - calc(var(--splitter-bar-size) / 2));");
+        styleText.Contains(",").IsFalse();
+    }
+
+    [TestCase("pl-PL")]
+    [TestCase("de-DE")]
+    public void GetPaneStyle_PercentMinSize_UsesInvariantDecimalSeparator(string cultureName)
+    {
+        CultureInfo.CurrentCulture = new CultureInfo(cultureName);
+        CultureInfo.CurrentUICulture = CultureInfo.CurrentCulture;
+
+        var splitContainer = new SplitContainer<double>
+        {
+            Orientation = SplitterOrientation.Vertical,
+            UnitOfPaneSize = UnitOfPaneSize.Percent,
+        };
+
+        var styleText = splitContainer.GetPaneStyle(null, 8.5);
+
+        styleText.Is("min-width:calc(8.5% - calc(var(--splitter-bar-size) / 2));flex:1;");
+        styleText.Contains(",").IsFalse();
+    }
+
+    // Pixel path is also culture-sensitive for fractional TSize.
+    // Latent today (most callers pass int), but the contract is "valid CSS".
+    [TestCase("pl-PL")]
+    [TestCase("de-DE")]
+    public void GetPaneStyle_PixelSize_FractionalDouble_UsesInvariantDecimalSeparator(string cultureName)
+    {
+        CultureInfo.CurrentCulture = new CultureInfo(cultureName);
+        CultureInfo.CurrentUICulture = CultureInfo.CurrentCulture;
+
+        var splitContainer = new SplitContainer<double>
+        {
+            Orientation = SplitterOrientation.Vertical,
+            UnitOfPaneSize = UnitOfPaneSize.Pixel,
+        };
+
+        var styleText = splitContainer.GetPaneStyle(627.5, null);
+
+        styleText.Is("width:627.5px;");
+        styleText.Contains(",").IsFalse();
     }
 }
